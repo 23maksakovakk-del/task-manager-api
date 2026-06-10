4370367e2f665f9214729a4908e4ea3895f4e04f
23maksakovakk-del <23maksakov.akk@gmail.com>
2026-06-10 21:33:09 +0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,30 @@
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index']);
+});
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller {
+    public function register(Request $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user'
+        ]);
+        return response()->json($user);
+    }
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return response()->json(compact('token'));
+    }
+}
